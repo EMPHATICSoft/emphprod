a9077cee9d42a130e04417f65a33bb61074a7845
NerKnutson <noahtknutson@gmail.com>
2025-11-25 16:15:24 -0600

    Adjusted good_runs_process.sh to accept a filename

--- emphproduction/scripts/good_runs_process.sh
+++ emphproduction/scripts/good_runs_process.sh
@@ -1,6 +1,8 @@
 #!/bin/sh
 [ -f ./batch_unpack.sh ] || { echo "Error: need batch_unpack.sh"; exit 1; }
-grep 'good' GoodRunsList.txt | sed 's/good//' | while read -r line; do
+FILE="${1}"
+[ -f "${FILE}" ] || exit "No good runs file specified"
+grep 'good' "${FILE}" | sed 's/good//' | while read -r line; do
 	RUN=$(echo ${line} | cut -f 1 -d ' ')
 	SUBRUN=$(echo ${line} | cut -f 2 -d ' ')
 	./batch_unpack.sh ${RUN} ${SUBRUN}
