7b76af0f5c7fa993332a21f430f7d4b3048889ad
Gavin S. Davies <gavin.s.davies@gmail.com>
2026-04-07 20:52:05 +0200

    use a run that we know works as default

--- emphgridutils/bin/generateMCJob.sh
+++ emphgridutils/bin/generateMCJob.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-FIRST_RUN=2293
+FIRST_RUN=2408
 timestamp=$(date +%N | cut -c 1-8 - | sed -E 's/0*(.*)/\1/g') #Last 8 digits of nanoseconds because GEANT only supports seeds up to 9x10^8
 RUN=$((PROCESS + FIRST_RUN))
 SEED=$((PROCESS + timestamp))
