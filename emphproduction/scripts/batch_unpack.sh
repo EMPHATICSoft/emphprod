f7977f53ab181c249ab2e111886aa7150f885e9d
NerKnutson <noahtknutson@gmail.com>
2026-04-15 17:17:08 -0500

    Adjusted defaults directories; source git tag from users app direcctory

--- emphproduction/scripts/batch_unpack.sh
+++ emphproduction/scripts/batch_unpack.sh
@@ -3,29 +3,36 @@
 FCL=$(mktemp XXXX.fcl)
 trap 'rm "$FCL"' EXIT
 
+USER=${USER:=nknutson}
+EMPHATIC_SOFT=${EMPHATIC_SOFT:=/exp/emph/app/users/${USER}/emphaticsoft}
+TAG=${TAG:=$(cd "${EMPHATIC_SOFT}" && git describe --exact-match --tags)}
+[ -z ${TAG} ] && { echo "Error: unable to determine tag"; exit 1; }
+
 if [ ${#} = 2 ]; then
 	RUN=${1}
 	SUBRUN=$(printf "%04.f" ${2})
 	[ -z "$(ls /exp/emph/data/raw/Mar2023/OutputData/emdaq_otsdaq_rootOutput_r${RUN}_s${SUBRUN}_*)" ] && { exit 1; }
-	OUTPUT_DIR="/exp/emph/data/users/nknutson/${RUN}"
+	OUTPUT_DIR="/exp/emph/data/users/${USER}/${RUN}"
 	mkdir -p ${OUTPUT_DIR}
 	for file in /exp/emph/data/raw/Mar2023/OutputData/emdaq_otsdaq_rootOutput_r${RUN}_s${SUBRUN}_*; do
 		LOG=$(mktemp "XXXX.log")
-		sed -e "s|fileNames: .*|fileNames: [\"${file}\"]|" '/exp/emph/app/users/nknutson/emphaticsoft/RawDataUnpacker/daq2rawdigit_job.fcl' > "${FCL}"
-		nice -n 20 art -c "${FCL}" -o "${OUTPUT_DIR}/emphdata_r${RUN}_s${SUBRUN}.artdaq.root" | tee --append "${LOG}"
+		sed -e "s|fileNames: .*|fileNames: [\"${file}\"]|" "/exp/emph/app/users/${USER}/emphaticsoft/RawDataUnpacker/daq2rawdigit_job.fcl" > "${FCL}"
+		nice -n 20 art -c "${FCL}" -o "${OUTPUT_DIR}/emphdata_${TAG}_r${RUN}_s${SUBRUN}.artdaq.root" | tee --append "${LOG}"
 	done
-	mv "${LOG}" "${OUTPUT_DIR}/r${RUN}_s${SUBRUN}.${LOG}"
+	mv "${LOG}" "${OUTPUT_DIR}/${TAG}_r${RUN}_s${SUBRUN}.${LOG}"
 elif [ ${#} = 1 ]; then
 	RUN=${1}
 	[ -z "$(ls /exp/emph/data/raw/Mar2023/OutputData/emdaq_otsdaq_rootOutput_r${RUN}_s*_*)" ] && { exit 1; }
-	OUTPUT_DIR="/exp/emph/data/users/nknutson/${RUN}"
+	OUTPUT_DIR="/exp/emph/data/users/${USER}/${RUN}"
 	mkdir -p ${OUTPUT_DIR}
 	for file in /exp/emph/data/raw/Mar2023/OutputData/emdaq_otsdaq_rootOutput_r${RUN}_s*_*; do
+		SUBRUN=${file##*_s}; SUBRUN=${SUBRUN%%_*}
 		LOG=$(mktemp "XXXX.log")
-		sed -e "s|fileNames: .*|fileNames: [\"${file}\"]|" '/exp/emph/app/users/nknutson/emphaticsoft/RawDataUnpacker/daq2rawdigit_job.fcl' > "${FCL}"
-		nice -n 20 art -c "${FCL}" -o "${OUTPUT_DIR}/emphdata_r${RUN}_s${SUBRUN}.artdaq.root" | tee --append ${LOG}
-		mv "${LOG}" "${OUTPUT_DIR}/r${RUN}.${LOG}"
+		sed -e "s|fileNames: .*|fileNames: [\"${file}\"]|" "/exp/emph/app/users/${USER}/emphaticsoft/RawDataUnpacker/daq2rawdigit_job.fcl" > "${FCL}"
+		nice -n 20 art -c "${FCL}" -o "${OUTPUT_DIR}/emphdata_${TAG}_r${RUN}_s${SUBRUN}.artdaq.root" | tee --append ${LOG}
+		mv "${LOG}" "${OUTPUT_DIR}/${TAG}_r${RUN}_s${SUBRUN}.${LOG}"
 	done
 else
 	echo "Usage: ./${0} <RUN> [Optional: <SUBRUN>]"
+	exit 1
 fi
